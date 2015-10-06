6183e430235865f34883b5e6a3d26d24926c35b0
Juha Lindstedt <juha@pakastin.fi>
2015-10-06 17:03:36 +0300

    added methods and events

--- dist/frzr.js
+++ dist/frzr.js
@@ -191,6 +191,7 @@ var frzr = (function () {
     options && this.setOptions(options);
     this.trigger('init', this);
     data && this.set(data);
+    this.trigger('inited', this);
   }
 
   inherits(View, Observable);
@@ -213,14 +214,42 @@ var frzr = (function () {
     };
   };
 
-  View.prototype.mount = function (target, sync) {
-    var $el = this.$el;
+  View.prototype.mount = function (target) {
+    var self = this;
+    var $el = self.$el;
 
-    this.root = target;
+    self.root = target;
 
     batchAnimationFrame(function () {
+      self.trigger('mount');
       target.appendChild($el);
+      self.trigger('mounted');
     });
+  };
+
+  View.prototype.unmount = function () {
+    var self = this;
+    var $el = self.$el;
+
+    if (!self.root) {
+      return;
+    }
+
+    batchAnimationFrame(function () {
+      self.trigger('unmount');
+      self.root.removeChild($el);
+      self.root = null;
+      self.trigger('unmounted');
+    });
+  };
+
+  View.prototype.destroy = function () {
+    var self = this;
+
+    self.trigger('destroy');
+    self.off();
+    self.unmount();
+    self.trigger('destroyed');
   };
 
   View.prototype.mountBefore = function (target, before) {
@@ -233,20 +262,19 @@ var frzr = (function () {
     });
   };
 
-  View.prototype.mountBeforeSync = function (target, before) {
-    var $el = this.$el;
-
-    this.root = target;
-
-    target.insertBefore($el, before);
-  };
-
   View.prototype.set = function (data) {
-    this.trigger('update', data);
+    var self = this;
+    batchAnimationFrame(function () {
+      self.trigger('render');
+    });
+    self.trigger('update', data);
     for (var key in data) {
-      this.data[key] = data[key];
+      self.data[key] = data[key];
     }
-    this.trigger('updated');
+    self.trigger('updated');
+    batchAnimationFrame(function () {
+      self.trigger('rendered');
+    });
   };
 
   View.prototype.setOptions = function (options) {
@@ -371,8 +399,15 @@ var frzr = (function () {
   inherits(Views, Observable);
 
   Views.prototype.mount = function (target) {
-    this.mounted = true;
     this.view.mount(target);
+  };
+
+  Views.prototype.mountBefore = function (target, before) {
+    this.view.mountBefore(target, before);
+  };
+
+  Views.prototype.unmount = function () {
+    this.view.unmount();
   };
 
   Views.prototype.reset = function (data, key) {
