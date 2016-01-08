e2aeba345fe44951df9f1ef50e2c9b932970af69
Juha Lindstedt <juha@pakastin.fi>
2016-01-08 14:54:05 +0200

    0.8.5

--- dist/frzr.js
+++ dist/frzr.js
@@ -206,32 +206,43 @@
   }
 
   function el (tagName, attributes) {
-    var element = document.createElement(tagName || 'div');
+    attributes = attributes || {};
+    if (tagName === 'text') {
+      var element = document.createTextNode(attributes.text || attributes.textContent || '');
+    } else if (attributes.svg) {
+      var element =  document.createElementNS("http://www.w3.org/2000/svg", "svg")
+    } else {
+      var element = document.createElement(tagName || 'div');
+    }
 
-    if (attributes) {
-      for (var key in attributes) {
-        if (key === 'text') {
-          element.textContent = attributes[key];
-        } else if (key === 'style') {
-          var styles = attributes.style.split(';');
-          for (var i = 0; i < styles.length; i++) {
-            var styleParts = styles[i].split(':');
-            if (styleParts.length > 1) {
-              element.style[styleParts[0].trim()] = styleParts[1].trim();
-            }
+    for (var key in attributes) {
+      if (key === 'text') {
+        element.textContent = attributes.text;
+      } else if (key === 'svg') {
+        continue;
+      } else if (key === 'style') {
+        var styles = attributes.style.split(';');
+        for (var i = 0; i < styles.length; i++) {
+          var styleParts = styles[i].split(':');
+          if (styleParts.length > 1) {
+            element.style[styleParts[0].trim()] = styleParts[1].trim();
           }
-        } else if (key === 'html') {
-          element.innerHTML = attributes[key];
-        } else {
-          element[key] = attributes[key];
         }
+      } else if (key === 'html') {
+        element.innerHTML = attributes[key];
+      } else {
+        element[key] = attributes[key];
       }
     }
     return element;
   }
 
-  function Observable () {
+  function Observable (options) {
     this.listeners = {};
+
+    for (var key in options) {
+      this[key] = options[key];
+    }
   }
 
   extend(Observable.prototype, {
